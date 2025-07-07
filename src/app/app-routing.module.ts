0867bed1c8c2e65e5afff24311f7f7b910b4f273
ShravanJump <138459242+ShravanJump@users.noreply.github.com>
2025-07-07 15:32:35 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { SignupComponent } from './signup/signup';
 import { Login} from './login/login';
-import { Table } from './table/table';
-import { Project} from './project/project';
-
 const routes: Routes = [
-  // Add your routes here, for example:
   { path: '', component: LandingComponent },
   { path: 'signup', component: SignupComponent},
-  { path: 'login', component: Login},
-  {path: 'table', component: Table},
-  {path: 'project', component: Project},
+  { path: 'login', component: Login}
   
 ];
 
@@ -21,3 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+

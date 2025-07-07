d6e0d6ac2269d54f1edbd25f5e3e27d34de52f01
ShravanJump <138459242+ShravanJump@users.noreply.github.com>
2025-07-07 15:14:51 +0530

    Update project.ts

--- src/app/project/project.ts
+++ src/app/project/project.ts
@@ -1,50 +1,47 @@
 import { Component } from '@angular/core';
-import { Sidebar } from '../sidebar/sidebar';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
 
-interface ProjectItem {
-  id: number;
+interface ProjectInfo {
   name: string;
+  logoUrl: string; // Placeholder for now
   manager: string;
   teamLead: string;
-  status: 'approved' | 'pending';
+  status: 'Approved' | 'Pending';
 }
 
 @Component({
   selector: 'app-project',
-  standalone: true,
-  imports: [Sidebar, CommonModule, RouterModule],
+   imports: [CommonModule,Sidebar, FormsModule],
   templateUrl: './project.html',
   styleUrls: ['./project.scss']
 })
-
-export class ProjectComponent {
-  projects: ProjectItem[] = [
+export class Project {
+  projects: ProjectInfo[] = [
     {
-      id: 1,
-      name: 'Project Alpha',
-      manager: 'Alice',
-      teamLead: 'Bob',
-      status: 'approved'
+      name: 'Slipstream',
+      logoUrl: 'https://tse2.mm.bing.net/th/id/OIP.8_IPFUVs9j--aeZ8nJQZVwAAAA?r=0&pid=ImgDet&w=150&h=150&c=7&dpr=1.3&o=7&rm=3://media-exp1.licdn.com/dms/image/C560BAQF46uKEa24-Cg/company-logo_200_200/0/1657222607808?e=2147483647&v=beta&t=IGJGwrMUzhLPeYFbjNH6tb9Yfs2vnhIBxv7aCzf2Hls',
+      manager: 'Alice Smith',
+      teamLead: 'Bob Johnson',
+      status: 'Approved'
     },
     {
-      id: 2,
-      name: 'Project Beta',
-      manager: 'Carol',
-      teamLead: 'Dave',
-      status: 'approved'
+      name: 'PSA BDP',
+      logoUrl: 'https://th.bing.com/th/id/OIP.x5oU8ZLsFIvEXILhMQAW5QHaEy?w=239&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3',
+      manager: 'Carol White',
+      teamLead: 'David Brown',
+      status: 'Approved'
     }
   ];
 
   addProject() {
-    const newId = this.projects.length + 1;
     this.projects.push({
-      id: newId,
-      name: `New Project ${newId}`,
-      manager: 'Manager Name',
-      teamLead: 'Team Lead Name',
-      status: 'pending'
+      name: 'New Project',
+      logoUrl: '',
+      manager: 'Pending',
+      teamLead: 'Pending',
+      status: 'Pending'
     });
   }
 }
